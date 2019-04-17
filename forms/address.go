2656f34d94ebe2a695e98f78d0a52448e8b0d02d
Thxzzzzz <july-7th@qq.com>
2019-04-17 23:06:45 +0800

    [todo]

--- forms/address.go
+++ forms/address.go
@@ -1,9 +1,10 @@
 package forms
 
-//地址表单 TODO 表单验证
+//地址表单 TODO 表单验证 默认地址
 type Address struct {
 	ID      uint64 `form:"id"`
 	Name    string `form:"name"`
 	Address string `form:"address"`
 	Phone   string `form:"phone"`
+	Default bool   `form:"default"`
 }
