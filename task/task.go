1fef5baef72368b901a1de112e49beaf17581750
Thxzzzzz <july-7th@qq.com>
2019-06-03 16:48:37 +0800

    定时器改为17点

--- task/task.go
+++ task/task.go
@@ -8,7 +8,7 @@ import (
 // 参考文档 https://beego.me/docs/module/toolbox.md#task
 func StartTask() {
 	// 每天0点执行
-	orderTask := toolbox.NewTask("orderTask", "0 0 0 * * *   ",
+	orderTask := toolbox.NewTask("orderTask", "0 0 17 * * *   ",
 		models.CloseFailureOrder)
 	toolbox.AddTask("orderTask", orderTask)
 	toolbox.StartTask()
