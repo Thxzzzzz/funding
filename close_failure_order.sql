1f2b51bf979745ba87df1fe22e0e6a0b24015004
Thxzzzzz <july-7th@qq.com>
2019-06-03 15:37:08 +0800

    [sql]

--- close_failure_order.sql
+++ close_failure_order.sql
@@ -1,5 +1,5 @@
 -- 开启事务
-BEGIN;
+-- BEGIN;
 
 -- 自动检查并退款
 UPDATE
@@ -7,11 +7,11 @@ orders  o
 LEFT JOIN products p
 ON o.product_id = p.id
 SET
-o.`status` = 7,
+o.status = 7,
 o.last_status = 2,
 o.refund_reason = "众筹失败，自动退款"
 WHERE
-o.`status` = 2
+o.status = 2
 AND p.end_time < DATE(NOW())
 AND p.current_price < p.target_price
 AND p.deleted_at IS NULL
@@ -22,7 +22,7 @@ SELECT *
 FROM orders;
 
 -- 回滚
-ROLLBACK;
+-- ROLLBACK;
  
 -- SELECT o.id
 -- FROM orders AS o
